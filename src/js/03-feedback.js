b243c7ccf944bcc933f9f1ea83e859b45491a676
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2024-04-09 21:44:04 +0300

    avatarblock

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,28 +14,18 @@ function EmptyObjectCreate() {
                     formData[form.elements[i].name] = "";
                     }}
     }
-function clearStorage() {
-    if (formData.check) {
-        form.reset();
-        console.log("formData is submited to backend", formData);
-        formData = null;
-        formData = {};
-        formDataMassive.lenght = 0;
-        localStorage.removeItem(LOCALSTORAGE_KEY);
-checkStorage();
-    }
-    else {
-       alert("Please accept subscribe")
-    }}
+
 function changeHandler() {
-    if (LOCALSTORAGE_KEY) {
-              CloneformData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-            for (let key in CloneformData) {
-            formData[key] = CloneformData[key];
-        }
-    } 
+    // if (LOCALSTORAGE_KEY) {
+    //     CloneformData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    //         for (let key in CloneformData) {
+    //             formData[key] = CloneformData[key];
+    //     }
+    // }
+    
     if (this.type !== 'checkbox' && this.type !== 'password') {
         formData[this.name] = this.value;
+         formData.password = "";
         localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
         formData.password = CurrentPassword;
         console.log("Localstorage", this.name, ":", formData[this.name] = this.value);
@@ -44,6 +34,8 @@ function changeHandler() {
     }
     else if (this.type === 'checkbox') {
         formData[this.name] = this.checked;
+        console.log("formData", formData);
+        formData.password = "";
         localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
         formData.password = CurrentPassword;
         console.log("check", formData[this.name]);
@@ -55,8 +47,6 @@ function changeHandler() {
          localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
         formData[this.name] = this.value;
         CurrentPassword = this.value;
-        // formData[this.name] = "sdds";
-       
         console.log("CurrentPassword", CurrentPassword);
         console.log("formData", formData);
         return
@@ -65,26 +55,59 @@ function changeHandler() {
 
 }
 function attachEvents() {
+           if (LOCALSTORAGE_KEY) {
+        CloneformData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+            for (let key in CloneformData) {
+                formData[key] = CloneformData[key];
+        } }
   for (let i = 0; i < formFields.length; i++) {
-  formFields[i].addEventListener('change', throttle(changeHandler, 500))
-    }
+      formFields[i].addEventListener('change', throttle(changeHandler, 500))
+   
+    }  console.log(formData);
 }
 
 function checkStorage() {
     const formFieldsSaved = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log("formFieldsSaved", formFieldsSaved);
     if (!formFieldsSaved) {
         EmptyObjectCreate();
     }
     else {
         for (let i in formFieldsSaved) {
             formDataMassive.push(formFieldsSaved[i]);
-            console.log( formDataMassive)
-       } for (let i = 0; i < formFields.length; ++i) {
-    if (formFields[i].type !== 'submit') 
-                formFields[i].value = formDataMassive[i];
-                console.log( formFields)
+        } console.log("formDataMassive", formDataMassive);
+        
+        for (let i = 0; i < formFields.length; ++i) {
+            if (formFields[i].type !== 'submit' && formFields[i].type !== 'button') {
+                
+                if (formFields[i].type === 'checkbox') {
+                    formFields[i].checked = formDataMassive[i];
+                     console.log("formDataMassive", formDataMassive[i]);
+                    console.log("checkbox", formFields[i].checked = formDataMassive[i]);
+                    console.log("checkbox", formFields[i].name);
+                }
+                else {
+                    formFields[i].value = formDataMassive[i];
+                    // console.log(formFields[i].name, formFields[i].value = formDataMassive[i]);
+                }
+      
+            }
         }
-} attachEvents();
     }
-   
+        attachEvents();
+    }
+function clearStorage() {
+    console.log("formData.check", formData.check)
+    if (formData.check) {
+        form.reset();
+        console.log("formData is submited to backend:", formData);
+        formData = null;
+        formData = {};
+        formDataMassive.lenght = 0;
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+checkStorage();
+    }
+    else {
+       alert("Please accept subscribe")
+    }}   
 checkStorage();
